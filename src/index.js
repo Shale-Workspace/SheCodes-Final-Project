c1700b130981320c85e1510dacdbcb184d672bb6
Smonds <102761494+Smonds@users.noreply.github.com>
2022-07-31 07:43:34 -0600

    Moved code to gitHub

---
+++ src/index.js
@@ -0,0 +1,21 @@
+let searchInput = document.querySelector("#search-text-input");
+const apiKey = "e441798f9c023ade80e4d84933e39e59";
+
+function search(event) {
+  event.preventDefault();
+
+  let h1 = document.querySelector("h1");
+
+  h1.innerHTML = `${searchInput.value} Weather`;
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTempature);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+function showTempature(response) {
+  console.log(response.data);
+  let h2 = document.querySelector("#currentTempature");
+  h2.innerHTML = `${Math.round(response.data.main.temp)}°C`;
+}
